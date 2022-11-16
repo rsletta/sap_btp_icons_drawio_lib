55c91e4fa3a634dc402adc6131caef9bdce389e5
Mauricio Lauffer <443888+mauriciolauffer@users.noreply.github.com>
2022-11-16 18:21:09 +1100

    feat: Get data from btp-service-metadata repo

--- src/index.js
+++ src/index.js
@@ -2,21 +2,35 @@ import { writeFile } from 'node:fs/promises';
 import xmlEscape from 'xml-escape';
 
 async function main() {
+    // Gets data from the official BTP service metadata repo
+    const baseUrl = `https://raw.githubusercontent.com/SAP-samples/btp-service-metadata/main/v0`;
+    const results = [];
+    const isUrl = new RegExp('^https?:\/\/');
     try {
-        const response = await fetch('https://discovery-center.cloud.sap/servicecatalog/Services?$format=json');
-        const odataResult = await response.json();
-        const mappedData = odataResult?.d?.results.map(item => {
-            const extendTags = `sap,btp,${item.Tags}`;
-            const tags = extendTags.split(',');
-            return {
-                "url": item.Icon,
-                "data": item.Icon,
+        const response = await fetch(`${baseUrl}/inventory.json`);
+        const inventory = await response.json();
+        //TODO: I know, a queue with concurrency would be much better. Next iteration maybe?
+        for (const file of inventory) {
+            const filename = `${baseUrl}/developer/${file.fileName}`;
+            console.log(filename);
+            const response = await fetch(filename);
+            results.push(await response.json());
+        }
+        const mappedData = results.map(item => {
+            const tags = `SAP,BTP,${item.name}`.split(',');
+            const shape = {
+                "url": item.icon,
+                "data": item.icon,
                 "w": 56,
                 "h": 56,
-                "title": xmlEscape(item.Name),
+                "title": xmlEscape(item.displayName),
                 "aspect": "fixed",
                 "tags": tags.map(tag => xmlEscape(tag))
+            };
+            if (!isUrl.test(item.icon)) {
+                shape.url = "";
             }
+            return shape;
         });
         const payload = `<mxlibrary>\n${JSON.stringify(mappedData)}\n</mxlibrary>`;
         await writeFile('./libs/SAP_BTP_Service_Icons_latest.xml', payload);
