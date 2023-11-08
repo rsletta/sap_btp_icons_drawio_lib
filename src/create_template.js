3f0f471d5e44f867e8216352a1125afd93e263ed
Jakob Kjaer <j.kjaer@sap.com>
2023-11-08 13:16:01 +1000

    update again

--- src/create_template.js
+++ src/create_template.js
@@ -2,7 +2,7 @@ import { cp } from 'node:fs/promises';
 import {setOutput } from '@actions/core';
 
 async function main() {
-  const path =  process.env.Title
+  const path =  process.env.Body
   console.log(path)
   setOutput("path", path);
 
