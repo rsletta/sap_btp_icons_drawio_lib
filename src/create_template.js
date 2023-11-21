2b4d283e99d341d505340f307a03a164bd8ab93a
Mauricio Lauffer <443888+mauriciolauffer@users.noreply.github.com>
2023-11-21 18:34:05 +1000

    chore: Add linting

--- src/create_template.js
+++ src/create_template.js
@@ -1,6 +1,5 @@
-import { appendFile, cp, mkdir, readFile, readdir, writeFile, rm } from 'node:fs/promises';
-import {setOutput } from '@actions/core';
-import {parse, toMd} from 'md-2-json'
+import {appendFile, cp, mkdir, readFile, readdir, writeFile, rm} from 'node:fs/promises';
+import {parse, toMd} from 'md-2-json';
 /**
  *Json structure of pull request
  *  {
@@ -10,50 +9,40 @@ import {parse, toMd} from 'md-2-json'
 }
  */
 async function main() {
-  const body =  process.env.Body
-  // const body = await readFile('./test/PR.txt', 'utf-8')
+  const body = process.env.Body;
+  const jsonBody = parse(body);
+  const templateName = jsonBody.Title.raw.replace(/\s/g, '');
+  const rawName = jsonBody.Title.raw.replace(/\n/g, '');
+  const files = await readdir('./upload/', {withFileTypes: true, encoding: 'utf-8'});
+  const regex = /\.xml$/;
+  if (files.length !== 2 || (!regex.test(files[0].name) && !regex.test(files[1].name))) {
+    throw new Error('File isn\'t uploaded into the upload directory or file is not XML');
+  }
 
-const jsonBody = parse(body)
-// console.log(jsonBody)
-const templateName = jsonBody.Title.raw.replace(/\s/g, "")
-const rawName = jsonBody.Title.raw.replace(/\n/g, "");
-const files = await readdir("./upload/", { withFileTypes: true, encoding: 'utf-8'})
-const regex = /\.xml$/
-if (files.length !== 2 || (!regex.test(files[0].name) && !regex.test(files[1].name))){
-  throw new Error("File isn't uploaded into the upload directory or file is not XML")
-} else {
   try {
-  const filename = files.find((file) => file.name.includes("xml"))?.name
-  const targetFileName = filename.replace(/\s/g, "")
-  const path = './src/templates/' +templateName
-  const mdFilePath = `./src/templates/${templateName}/${templateName}.md`
+    const filename = files.find((file) => file.name.includes('xml'))?.name;
+    const targetFileName = filename.replace(/\s/g, '');
+    const path = `./src/templates/${templateName}`;
+    const mdFilePath = `./src/templates/${templateName}/${templateName}.md`;
 
-  await mkdir(path)
-  await cp('./upload/' +filename, path +"/" +targetFileName)
-    await writeFile(mdFilePath,body)
-  await appendFile(mdFilePath, `\n\n
-  [Open Diagram](https://app.diagrams.net/?create=https://raw.githubusercontent.com/${process.env.Repo}/main/src/templates/${templateName}/${filename}&clibs=Uhttps://raw.githubusercontent.com/mauriciolauffer/sap_btp_icons_drawio_lib/main/libs/SAP_BTP_Service_Icons_latest.xml)`)
+    await mkdir(path);
+    await cp(`./upload/${filename}${path}/${targetFileName}`);
+    await writeFile(mdFilePath, body);
+    await appendFile(mdFilePath, `\n\n
+[Open Diagram in the browser](https://app.diagrams.net/?create=https://raw.githubusercontent.com/${process.env.Repo}/main/src/templates/${templateName}/${filename}&clibs=Uhttps://raw.githubusercontent.com/rsletta/sap_btp_icons_drawio_lib/main/libs/SAP_BTP_Service_Icons_latest.xml)`);
 
-  await appendFile(mdFilePath, `\n\n## Desktop Client Draw.io\n\n
-  To use in desktop client use the following link and import with template url: \n
-  https://raw.githubusercontent.com/${process.env.Repo}/main/src/templates/${templateName}/${filename}&clibs=Uhttps://raw.githubusercontent.com/mauriciolauffer/sap_btp_icons_drawio_lib/main/libs/SAP_BTP_Service_Icons_latest.xml)`)
+    await appendFile(mdFilePath, `\n\n## Desktop Client Draw.io\n
+To use in desktop client use the following link and import with template url: \n
+https://raw.githubusercontent.com/${process.env.Repo}/main/src/templates/${templateName}/${filename}&clibs=Uhttps://raw.githubusercontent.com/rsletta/sap_btp_icons_drawio_lib/main/libs/SAP_BTP_Service_Icons_latest.xml`);
 
-  await rm('./upload/' +filename)
-  const summary = await readFile('./src/SUMMARY.md', 'utf-8')
-  let summaryBody = parse(summary)
-  summaryBody.Templates.raw = summaryBody.Templates.raw +`- [${rawName}](templates/${templateName}/${templateName}.md)\n\n`
-  await writeFile('./src/SUMMARY.md', toMd(summaryBody))
-
-  }
-  catch (e) {
-    console.log(e)
-    throw new Error('Something went wrong!')
+    await rm(`./upload/${filename}`);
+    const summary = await readFile('./src/SUMMARY.md', 'utf-8');
+    const summaryBody = parse(summary);
+    summaryBody.Templates.raw = `${summaryBody.Templates.raw}- [${rawName}](templates/${templateName}/${templateName}.md)\n\n`;
+    await writeFile('./src/SUMMARY.md', toMd(summaryBody));
+  } catch (err) {
+    throw new Error(err);
   }
 }
- 
-
-}
-
-
 
 main();
--- src/index.js
+++ src/index.js
@@ -1,43 +1,44 @@
-import { writeFile } from 'node:fs/promises';
+import {writeFile} from 'node:fs/promises';
 import xmlEscape from 'xml-escape';
 
+/**
+ *
+ */
 async function main() {
-    // Gets data from the official BTP service metadata repo
-    const baseUrl = `https://raw.githubusercontent.com/SAP-samples/btp-service-metadata/main/v0`;
-    const results = [];
-    const isUrl = new RegExp('^https?:\/\/');
-    try {
-        const response = await fetch(`${baseUrl}/inventory.json`);
-        const inventory = await response.json();
-        //TODO: I know, a queue with concurrency would be much better. Next iteration maybe?
-        for (const file of inventory) {
-            const filename = `${baseUrl}/developer/${file.fileName}`;
-            console.log(filename);
-            const response = await fetch(filename);
-            results.push(await response.json());
-        }
-        const mappedData = results.map(item => {
-            const tags = `SAP,BTP,${item.name}`.split(',');
-            const shape = {
-                "url": item.icon,
-                "data": item.icon,
-                "w": 56,
-                "h": 56,
-                "title": xmlEscape(item.displayName),
-                "aspect": "fixed",
-                "tags": tags.map(tag => xmlEscape(tag))
-            };
-            if (!isUrl.test(item.icon)) {
-                shape.url = "";
-            }
-            return shape;
-        });
-        const payload = `<mxlibrary>\n${JSON.stringify(mappedData)}\n</mxlibrary>`;
-        await writeFile('./src/libs/SAP_BTP_Service_Icons_latest.xml', payload);
-
-    } catch (err) {
-        console.error(err);
+  // Gets data from the official BTP service metadata repo
+  const baseUrl = `https://raw.githubusercontent.com/SAP-samples/btp-service-metadata/main/v0`;
+  const results = [];
+  const isUrl = new RegExp('^https?://');
+  try {
+    const response = await fetch(`${baseUrl}/inventory.json`);
+    const inventory = await response.json();
+    // TODO: I know, a queue with concurrency would be much better. Next iteration maybe?
+    for (const file of inventory) {
+      const filename = `${baseUrl}/developer/${file.fileName}`;
+      const response = await fetch(filename);
+      results.push(await response.json());
     }
+    const mappedData = results.map((item) => {
+      const tags = `SAP,BTP,${item.name}`.split(',');
+      const shape = {
+        'url': item.icon,
+        'data': item.icon,
+        'w': 56,
+        'h': 56,
+        'title': xmlEscape(item.displayName),
+        'aspect': 'fixed',
+        'tags': tags.map((tag) => xmlEscape(tag))
+      };
+      if (!isUrl.test(item.icon)) {
+        shape.url = '';
+      }
+      return shape;
+    });
+    const payload = `<mxlibrary>\n${JSON.stringify(mappedData)}\n</mxlibrary>`;
+    await writeFile('./src/libs/SAP_BTP_Service_Icons_latest.xml', payload);
+  } catch (err) {
+    throw new Error(err);
+  }
 }
 
 main();
